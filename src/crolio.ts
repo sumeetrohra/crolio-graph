4c9f8f21508c0cef14bdff042a8d959f51fdf2c8
rohrasumeet <sumetrohra1@gmail.com>
2022-12-04 01:26:15 +0530

    fin

--- src/crolio.ts
+++ src/crolio.ts
@@ -1,47 +1,52 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
   BucketCreated as BucketCreatedEvent,
   InvestedInBucket as InvestedInBucketEvent,
-  WithdrawnFromBucket as WithdrawnFromBucketEvent
-} from "../generated/Crolio/Crolio"
+  WithdrawnFromBucket as WithdrawnFromBucketEvent,
+} from "../generated/Crolio/Crolio";
 import {
-  BucketCreated,
+  Bucket,
   InvestedInBucket,
-  WithdrawnFromBucket
-} from "../generated/schema"
+  WithdrawnFromBucket,
+} from "../generated/schema";
 
 export function handleBucketCreated(event: BucketCreatedEvent): void {
-  let entity = new BucketCreated(
+  let entity = new Bucket(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bucketId = event.params.bucketId
-  entity.bucketName = event.params.bucketName
-  entity.description = event.params.description
-  entity.tokens = event.params.tokens
-  entity.weightages = event.params.weightages
-  entity.creator = event.params.creator
+  );
+  entity.bucketId = event.params.bucketId;
+  entity.bucketName = event.params.bucketName;
+  entity.description = event.params.description;
+  entity.tokens = event.params.tokens.map<Bytes>((i) =>
+    Bytes.fromHexString(i.toHexString())
+  );
+  entity.weightages = event.params.weightages;
+  entity.creator = event.params.creator;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleInvestedInBucket(event: InvestedInBucketEvent): void {
   let entity = new InvestedInBucket(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bucketId = event.params.bucketId
-  entity.amountInvested = event.params.amountInvested
-  entity.investorAddress = event.params.investorAddress
-  entity.tokens = event.params.tokens
-  entity.holdingsBought = event.params.holdingsBought
+  );
+  entity.bucketId = event.params.bucketId;
+  entity.amountInvested = event.params.amountInvested;
+  entity.investorAddress = event.params.investorAddress;
+  entity.tokens = event.params.tokens.map<Bytes>((i) =>
+    Bytes.fromHexString(i.toHexString())
+  );
+  entity.holdingsBought = event.params.holdingsBought;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleWithdrawnFromBucket(
@@ -49,16 +54,18 @@ export function handleWithdrawnFromBucket(
 ): void {
   let entity = new WithdrawnFromBucket(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.bucketId = event.params.bucketId
-  entity.amountOut = event.params.amountOut
-  entity.investorAddress = event.params.investorAddress
-  entity.tokens = event.params.tokens
-  entity.holdingsSold = event.params.holdingsSold
+  );
+  entity.bucketId = event.params.bucketId;
+  entity.amountOut = event.params.amountOut;
+  entity.investorAddress = event.params.investorAddress;
+  entity.tokens = event.params.tokens.map<Bytes>((i) =>
+    Bytes.fromHexString(i.toHexString())
+  );
+  entity.holdingsSold = event.params.holdingsSold;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
